54f2afe36521fa0df209309a07f8fbfe7a5c8ffc
Alex Cannizzaro <110434191+abcannizz@users.noreply.github.com>
2025-01-29 19:15:04 -0800

    added GuessManager class

---
+++ Cpsc370Final/GuessManager.cs
@@ -0,0 +1,11 @@
+namespace Cpsc370Final;
+
+public class GuessManager
+{
+    private List<char> guessedLetters;
+    
+    public GuessManager()
+    {
+        guessedLetters = new List<char>();
+    }
+}
